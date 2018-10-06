2a4386b1f97ed026b22a1549da05bdd45ee73277
SchneiderMan <jschneid94@gmail.com>
2018-10-05 18:05:57 -0700

    Initialized server and routes

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+
+app.use(bodyParse.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("Listening on PORT: " + PORT);
+})
